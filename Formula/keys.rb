5481d39d4cd8fadc12566b7d4e9ccafbb2b9e90e
Satyam Raj <satyammast@gmail.com>
2026-02-28 21:40:13 +0530

    Update keys to v0.2.0

--- Formula/keys.rb
+++ Formula/keys.rb
@@ -1,8 +1,8 @@
 class Keys < Formula
   desc "Manage API keys locally from the terminal"
   homepage "https://github.com/stym06/keys"
-  url "https://github.com/stym06/keys/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e51b35a2d44b6f4c817a67967cae239e209d538ce2c3b1b9fa5e77f21e474707"
+  url "https://github.com/stym06/keys/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "1718c075aebfb7d0a5c190850b87f60b0e9a1040704c29d47621129e548d7741"
   license "MIT"
 
   depends_on "go" => :build
